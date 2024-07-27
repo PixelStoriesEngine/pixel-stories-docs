9dc7aa60f0f148c3ab341a326e5abf63d98f1c86
tdgao <mr.trumgao@gmail.com>
2024-07-26 21:57:41 -0700

    add tailwind colors and more styles on pricing plan page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,23 @@ import starlightPlugin from "@astrojs/starlight-tailwind";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "rgb(var(--color-primary) / <alpha-value>)",
+        text: "--sl-color-text",
+        "color-accent-low": "var(--sl-color-accent-low)",
+        "color-accent": "var(--sl-color-accent)",
+        "color-accent-high": "var(--sl-color-accent-high)",
+        "color-white": "var(--sl-color-white)",
+        "color-gray-1": "var(--sl-color-gray-1)",
+        "color-gray-2": "var(--sl-color-gray-2)",
+        "color-gray-3": "var(--sl-color-gray-3)",
+        "color-gray-4": "var(--sl-color-gray-4)",
+        "color-gray-5": "var(--sl-color-gray-5)",
+        "color-gray-6": "var(--sl-color-gray-6)",
+        "color-black": "var(--sl-color-black)",
+      },
+    },
   },
   plugins: [starlightPlugin()],
 };
