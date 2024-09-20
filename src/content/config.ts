dabe22d3b0af02d44f13f317e10187af30d80e6d
tdgao <mr.trumgao@gmail.com>
2024-09-20 16:46:05 -0700

    move blog schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection, z, type SchemaContext } from "astro:content";
 import { docsSchema } from "@astrojs/starlight/schema";
-import { blogSchema } from "./blogSchema";
+import { blogSchema } from "../blogSchema";
 
 export const collections = {
   docs: defineCollection({
