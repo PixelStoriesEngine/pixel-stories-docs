5e7fa8a9daec82c97a3675862aa7aa21b9a3ca53
tdgao <mr.trumgao@gmail.com>
2025-09-12 12:02:39 -0700

    update config.ts for astro 5

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,15 @@
 import { defineCollection, z, type SchemaContext } from "astro:content";
+import { docsLoader } from "@astrojs/starlight/loaders";
 import { docsSchema } from "@astrojs/starlight/schema";
-import { blogSchema } from "../blogSchema";
+import { blogSchema } from "./blogSchema";
 
 export const collections = {
   docs: defineCollection({
+    loader: docsLoader(),
     schema: docsSchema(),
   }),
   blog: defineCollection({
+    loader: docsLoader(),
     schema: docsSchema({
       extend: (context) => blogSchema(context),
     }),
