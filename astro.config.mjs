3eec7dc8a8587ed1803c50f59ee5562a851292b3
tdgao <mr.trumgao@gmail.com>
2025-09-12 11:53:44 -0700

    fix favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -41,6 +41,42 @@ export default defineConfig({
         "./src/styles/tailwind.css",
         "./src/styles/custom.css",
       ],
+      favicon: "/favicon.ico",
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            rel: "apple-touch-icon",
+            sizes: "180x180",
+            href: "/apple-touch-icon.png",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            href: "/favicon-32x32.png",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            href: "/favicon-16x16.png",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "manifest",
+            href: "/site.webmanifest",
+          },
+        },
+      ],
       sidebar: [
         {
           label: "Getting started",
