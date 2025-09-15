2bf3c5e6bac67506490b414eb8e1770867555206
tdgao <mr.trumgao@gmail.com>
2025-09-15 10:40:35 -0700

    fix incorrect migration for blog content

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,6 +2,7 @@ import { defineCollection, z, type SchemaContext } from "astro:content";
 import { docsLoader } from "@astrojs/starlight/loaders";
 import { docsSchema } from "@astrojs/starlight/schema";
 import { blogSchema } from "./blogSchema";
+import { glob } from "astro/loaders";
 
 export const collections = {
   docs: defineCollection({
@@ -9,7 +10,10 @@ export const collections = {
     schema: docsSchema(),
   }),
   blog: defineCollection({
-    loader: docsLoader(),
+    loader: glob({
+      pattern: ["**/*.md", "**/*.mdx"],
+      base: "./src/content/blog",
+    }),
     schema: docsSchema({
       extend: (context) => blogSchema(context),
     }),
