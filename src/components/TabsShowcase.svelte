cf9eaf306378a994da2bac9320045e86a01b406f
tdgao <mr.trumgao@gmail.com>
2025-10-19 12:57:33 -0700

    add ".not-content" on img

--- src/components/TabsShowcase.svelte
+++ src/components/TabsShowcase.svelte
@@ -86,7 +86,7 @@
           aria-labelledby={"tab-" + i}
           src={current.imgSrc.src}
           alt={current.alt ?? current.title}
-          class="w-full rounded-2xl shadow-xl object-contain"
+          class="not-content w-full rounded-2xl shadow-xl object-contain"
         />
       </div>
     {/each}
